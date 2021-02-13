76d5f95d9d91ea170dd87f6327d86dc3804ef7c8
Simon Gasse <sgasse@users.noreply.github.com>
2021-02-13 16:23:50 +0100

    Refactor node adding functions

--- src/main.rs
+++ src/main.rs
@@ -15,64 +15,40 @@ impl Graph {
         Graph { names: Vec::new(), parents: Vec::new(), children: Vec::new(), name2idx: HashMap::new() }
     }
 
-    fn add_with_parent_r(&mut self, name: String, parent: Option<String>) -> Result<usize, &str> {
+    fn add_to_parent(&mut self, child: String, parent: String) -> Result<usize, &str> {
+        // Parent node exists
+        let p_idx = *(self.name2idx.get(&parent).unwrap());
+
         let node_idx = self.parents.len();
 
-        match parent {
-            Some(p) => {
-                match self.name2idx.get(&p) {
-                    Some(&p_idx) => {
-                        self.parents.push(Some(p_idx));
-                        // Update children of parent
-                        self.children[p_idx].push(node_idx);
-                    },
-                    None => return Err("Unknown key"),
-                }
-            },
-            None => self.parents.push(None),
+        // Check if child node already exists
+        if self.names.contains(&Box::new(child.clone())) {
+            return Err("Child already exists");
         }
 
-        let name_ = name.clone();
-
-        // Add new node with parent
-        self.names.push(Box::new(name));
+        self.names.push(Box::new(child.clone()));
+        self.name2idx.insert(child.clone(), node_idx);
+        self.parents.push(Some(p_idx));
         self.children.push(Vec::new());
-
-        self.name2idx.insert(name_, node_idx);
+        self.children[p_idx].push(node_idx);
         Ok(node_idx)
-    }
-
-    fn add_with_parent(&mut self, name: String, parent: Option<String>) {
-        match self.add_with_parent_r(name, parent) {
-            Ok(node_idx) => println!("Added node {}", node_idx),
-            Err(msg) => println!("Fail: {}", msg),
-        }
     }
 
     fn add_with_children(&mut self, name: String, children: Vec<String>) {
         let node_idx = self.parents.len();
 
         // Check if parent is already there
-        match self.names.contains(&Box::new(name.clone())) {
-            true => {
-                // Parent node exists already - check for children
-                // Add children
-                for child in children.iter() {
-                    self.add_with_parent((*child).clone(), Some(name.clone()));
-                }
-            },
-            false => {
-                // Add parent node
-                self.names.push(Box::new(name.clone()));
-                self.parents.push(None);
-                self.name2idx.insert(name.clone(), node_idx);
-                self.children.push(Vec::new());
+        if !self.names.contains(&Box::new(name.clone())) {
+            // Parent node does not exist - create
+            self.names.push(Box::new(name.clone()));
+            self.parents.push(None);
+            self.name2idx.insert(name.clone(), node_idx);
+            self.children.push(Vec::new());
+        }
 
-                // Add children
-                for child in children.iter() {
-                    self.add_with_parent((*child).clone(), Some(name.clone()));
-                }
-            },
+        // Add children
+        for child in children.iter() {
+            let _ = self.add_to_parent((*child).clone(), name.clone());
         }
     }
 }
@@ -81,13 +57,7 @@ impl Graph {
 fn main() {
     println!("A graph of nodes in Rust!");
 
-    let mut gr = Graph::new();
-
-    gr.add_with_parent("A".to_string(), None);
-    gr.add_with_parent("B".to_string(), Some("A".to_string()));
-    gr.add_with_parent("C".to_string(), Some("A".to_string()));
-
-    println!("{:?}", gr);
+    // let mut gr = Graph::new();
 }
 
 
@@ -95,7 +65,7 @@ fn main() {
 mod test {
     use super::Graph;
 
-    fn graph_fields_eq_len(graph: &Graph, len: usize) {
+    fn assert_graph_field_len(graph: &Graph, len: usize) {
         assert_eq!(graph.names.len(), len);
         assert_eq!(graph.parents.len(), len);
         assert_eq!(graph.children.len(), len);
@@ -109,64 +79,71 @@ mod test {
     }
 
     #[test]
-    fn test_add_with_children() {
-        let mut gr = Graph::new();
-        let parent = "A".to_string();
-        let children = vec!["B".to_string(), "C".to_string(), "D".to_string()];
-        gr.add_with_children(parent, children);
-
-        assert_eq!(gr.names.len(), 4);
-        assert_eq!(gr.children[0].len(), 3);
-    }
-
-    #[test]
-    fn initialize_graph() {
+    fn test_graph_new() {
         let gr = Graph::new();
-        graph_fields_eq_len(&gr, 0);
+        assert_graph_field_len(&gr, 0);
     }
 
     #[test]
     fn test_add_with_parent_r() {
-        let mut gr = Graph::new();
+        //let mut gr = Graph::new();
 
         // Add a node without parent
-        let mut res = gr.add_with_parent_r("A".to_string(), None);
-        assert_eq!(res, Ok(0));
 
         // Add a node with an existing parent
-        res = gr.add_with_parent_r("B".to_string(), Some("A".to_string()));
-        assert_eq!(res, Ok(1));
 
         // Try to add a node with a non-existant parent
-        graph_fields_eq_len(&gr, 2);
-        res = gr.add_with_parent_r("Test".to_string(), Some("NonExistant".to_string()));
-        assert_eq!(res, Err("Unknown key"));
-        graph_fields_eq_len(&gr, 2);
 
         // Try adding a node with spaces and unicode name
-        res = gr.add_with_parent_r("Wéird ßpäßes".to_string(), Some("B".to_string()));
-        assert_eq!(res, Ok(2));
         
         // Reference the weird node
-        res = gr.add_with_parent_r("C".to_string(), Some("Wéird ßpäßes".to_string()));
-        assert_eq!(res, Ok(3));
-        graph_fields_eq_len(&gr, 4);
     }
 
     #[test]
     fn test_add_with_parent() {
         // To see the output, run with `-- --nocapture`
+        //let mut gr = Graph::new();
+
+        // Add a node without parent
+
+        // Add a node with a parent
+
+        // Add a node without a parent
+    }
+
+
+    #[test]
+    fn test_add_to_parent() {
         let mut gr = Graph::new();
 
         // Add a node without parent
-        gr.add_with_parent("A".to_string(), None);
+        gr.add_with_children("A".to_string(), Vec::new());
 
-        // Add a node with a parent
-        gr.add_with_parent("B".to_string(), Some("A".to_string()));
+        // Add children to root node
+        let _ = gr.add_to_parent("B".to_string(), "A".to_string());
+        assert_graph_field_len(&gr, 2);
+        assert_eq!(gr.children[0].len(), 1);
+    }
 
-        // Add a node without a parent
-        gr.add_with_parent("C".to_string(), Some("NonExistant".to_string()));
+    #[test]
+    fn test_add_with_children() {
+        let mut gr = Graph::new();
 
+        // Add node without parent
+        gr.add_with_children("Root".to_string(), Vec::new());
+        assert_graph_field_len(&gr, 1);
+
+        // Add children to existing parent
+        gr.add_with_children("Root".to_string(), vec!["A".to_string()]);
+        assert_graph_field_len(&gr, 2);
+
+        // Add more children
+        let parent = "A".to_string();
+        let children = vec!["B".to_string(), "C".to_string(), "D".to_string()];
+        gr.add_with_children(parent, children);
+
+        assert_graph_field_len(&gr, 5);
+        assert_eq!(gr.children[1].len(), 3);
     }
 }
 
@@ -176,3 +153,4 @@ mod test {
 // Downstream
 // Searches
 // Reject existing node
+// Lookup in hashmap instead of vec
